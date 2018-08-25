8c7a3f2ccc8b213b22b636ef070dc3af8645db52
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-08-24 22:04:00 -0300

    Icon added

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// main application object
+function App() {
+    this.loadImg = function () {
+
+    }
+};
+
+// when the opencv.js was fully loaded
+$("#opencvJSFile").ready(function(){
+    var app = new App();
+
+});
