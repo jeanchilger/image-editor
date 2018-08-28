87d412477cdaee28a4bfe87fc2c0c2beceae0596
aluno-uffs <Aluno UFFS>
2018-08-28 17:44:19 -0300

    sla

--- js/main.js
+++ js/main.js
@@ -2,11 +2,15 @@
 function App() {
     this.loadImg = function () {
 
+    },
+
+    this.fuck = function () {
+        alert("Fuck You!");
     }
 };
 
 // when the opencv.js was fully loaded
 $("#opencvJSFile").ready(function(){
     var app = new App();
-    
+    app.fuck();
 });
