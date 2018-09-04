9c5cf6c5f054eaaf9b3f251b64b9ee91d2ebc741
JeanCHilger <hilgerjeancarlo@gmail.com>
2018-09-03 22:25:23 -0300

    Read img from input

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
-// main application object
-function App(name) {
-    this.loadImg = function () {
+function App() {
+    // main application 'object'
+    this.loadImgFromFile = function (event) {
+        /*
+        Load the image from input file.
+        There is a better way??
+        */
+        var tgt = event.target;
+        var fr = new FileReader();
+
+        fr.onload = function () {
+            $("#inputImg").attr("src", fr.result);
+        }
+
+        fr.readAsDataURL(tgt.files[0]);
 
     },
 
-    this.fuck = function () {
-        alert("Fuck You!");
+    this.loadImgToCV = function () {
+        let imgElement = document.getElementById("inputImg");
+        imgElement.onload = function () {
+            let mat = cv.imread(imgElement);
+            cv.imshow("outputImg", mat);
+            mat.delete();
+        };
     }
+
 };
 
 // when the opencv.js was fully loaded
 $("#opencvJSFile").ready(function () {
     var app = new App();
-    var c = document.getElementById('mainDraw');
-    var ctx = c.getContext("2d");
-    console.log(ctx);
+
+    // Load the image
+    $("#fileInput").change(function(event) {
+        app.loadImgFromFile(event);
+        app.loadImgToCV();
+    });
+
 });
